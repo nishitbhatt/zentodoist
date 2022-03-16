4e475819449f7232de8b2519baaee15001660e48
unknown <nishit.bhatt96@gmail.com>
2022-03-16 19:33:36 +0530

    Changed Package.json

--- app/connection.js
+++ app/connection.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
-const databaseString = process.env.DATABASESTRINGPRODUCTION;
+// const databaseString = process.env.DATABASESTRINGPRODUCTION;
+const databaseString = "mongodb+srv://TaskManager:Paparocks_123@cluster0.3rcip.mongodb.net/test";
 
 mongoose.connect(databaseString).then((data) => {
     console.log('Connction successfull !');

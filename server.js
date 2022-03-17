7ce8df11a0ad4b970ca80fff684918511de05f1d
unknown <nishit.bhatt96@gmail.com>
2022-03-17 12:39:24 +0530

    Changed Package.json

--- server.js
+++ server.js
@@ -19,12 +19,16 @@ var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 const todoRoutes = require('./app/routers/todo.routes');
 
+app.disable('etag');
+
 // Allow CORS For Localhost
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
-    res.header("Cache-Control", "no-cache");
+    res.header("Cache-Control", "no-cache, no-store, must-revalidate");
+    res.header("Pragma", "no-cache");
+    res.header("Expires", 0);
     next();
 });
 // Todo Routes
